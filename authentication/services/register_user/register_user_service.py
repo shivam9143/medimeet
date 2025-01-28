5ffe997df8e91067de2368b08e15957522d07ea5
Shivam Kanodia <107026221+shivamkanodia9143@users.noreply.github.com>
2025-01-28 16:47:27 +0530

    fix save user

--- authentication/services/register_user/register_user_service.py
+++ authentication/services/register_user/register_user_service.py
@@ -17,8 +17,7 @@ class UserRegistrationService:
             mobile_number = serializer.validated_data['mobile_number']
 
             response = self.otp_sending_service.handle_otp_sending(mobile_number)
-            if response:  # If response exists, return it (indicates error or OTP sent)
-                return response
-
+            if response.get("code") == status.HTTP_201_CREATED:
+                serializer.save(is_verified=False)
         return create_response(code=status.HTTP_400_BAD_REQUEST, error=str(serializer.errors),
                                message="Something went wrong!")
