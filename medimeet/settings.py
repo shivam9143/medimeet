d0802a8d2be4f08f502d35e780df6ca5cf633f2e
Shivam Kanodia <107026221+shivamkanodia9143@users.noreply.github.com>
2025-01-28 11:59:57 +0530

    updated local db to aws rd

--- medimeet/settings.py
+++ medimeet/settings.py
@@ -119,10 +119,21 @@ WSGI_APPLICATION = 'medimeet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'finance_buddy',  # Reuse the existing database name
+        'USER': 'sihvamkanodia',
+        'PASSWORD': 'RollingBanners1139$%#',
+        'HOST': 'finance-buddy-db-1.clyya82caypg.ap-south-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
